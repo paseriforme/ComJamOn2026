dab57820b81c409b2af7d56da40c7dd77392cc1f
Andrés García Navarro <andrga37@ucm.es>
2026-02-20 19:23:12 +0100

    esta un poco regular

--- godot-skeleton-main/scripts/game/gameGuitar.gd
+++ godot-skeleton-main/scripts/game/gameGuitar.gd
@@ -7,14 +7,21 @@ var pool_pulsos := []
 var actual_pulso := 0
 
 func _ready() -> void:
-	for i in range(2):
+	for i in range(3):
 		var pulso = PULSO.instantiate()
 		add_child(pulso)
 		pool_pulsos.push_back(pulso)
 
 func next_pulse():
+	var tween1 = pool_pulsos[actual_pulso].create_tween()
+	tween1.tween_property(pool_pulsos[actual_pulso], "rotation_degrees", 220, 0.5)
 	actual_pulso +=1
+	
 	if actual_pulso >= len(pool_pulsos): actual_pulso = 0
+	
+	var tween2 = pool_pulsos[actual_pulso].create_tween()
+	tween2.tween_property(pool_pulsos[actual_pulso], "rotation_degrees", 45, 0.5)
 
 func _physics_process(delta: float) -> void:
-	next_pulse()
+	if Input.is_action_pressed("rasgar",true):
+		next_pulse()
