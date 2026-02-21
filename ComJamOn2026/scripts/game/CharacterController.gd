7060c9fbdfb08a89c4497ae5ef5c356f9ce8e71e
Cynthia Tristán <cyntrist@ucm.es>
2026-02-21 21:34:27 +0100

    sprites 8 direcciones

--- ComJamOn2026/scripts/game/CharacterController.gd
+++ ComJamOn2026/scripts/game/CharacterController.gd
@@ -3,9 +3,11 @@ class_name CharacterController
 
 @export var speed : float = 10
 
+# N, NE, E, SE, S, SW, W, NW
 var directions := [Vector2(0,1), Vector2(0.75,0.75),Vector2(1,0), Vector2(0.75,-0.75),Vector2(0,-1), Vector2(-0.75,-0.75),Vector2(-1,0), Vector2(-0.75,0.75)]
+@export var sprite_directions : Array[Texture2D] = [null, null, null, null, null, null, null, null]
 var direction := 0
-
+	
 var redPressed = false;
 var greenPressed = false;
 var strumPressed = false;
@@ -37,6 +39,7 @@ func _physics_process(delta: float) -> void:
 	if not greenPressed and Input.is_action_pressed("verde",true):
 		greenPressed = true
 		#animated_sprite_2d.play("face_down")
+		$Sprite2D.texture = sprite_directions[direction]
 		direction += 1
 		if direction >= len(directions): 
 			direction = 0
@@ -47,6 +50,7 @@ func _physics_process(delta: float) -> void:
 	if not redPressed and Input.is_action_pressed("rojo",true):
 		redPressed = true
 		#animated_sprite_2d.play("face_down")
+		$Sprite2D.texture = sprite_directions[direction]
 		direction -= 1
 		if direction < 0: 
 			direction = len(directions) -1
