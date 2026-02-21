4cb65a981aab4b30eead14bb29db981e43cc9158
Andrés García Navarro <andrga37@ucm.es>
2026-02-21 20:31:02 +0100

    hola cycy, te devuelvo el controller a como estaba perdon

--- ComJamOn2026/scripts/game/CharacterController.gd
+++ ComJamOn2026/scripts/game/CharacterController.gd
@@ -6,73 +6,45 @@ class_name CharacterController
 var directions := [Vector2(0,1), Vector2(0.75,0.75),Vector2(1,0), Vector2(0.75,-0.75),Vector2(0,-1), Vector2(-0.75,-0.75),Vector2(-1,0), Vector2(-0.75,0.75)]
 var direction := 0
 
-var redPressed = false;
-var greenPressed = false;
-var yellowPressed = false;
-var bluePressed = false;
-var orangePressed = false;
 var strumPressed = false;
 var canwalk = false;
+
+var init_scale;
+var factor_spr_scale : Vector2 = Vector2(0.2, 0.2);
+var trans_scale = Tween.TRANS_QUAD
+
+var time_steps: float = 0.3
+var factor_steps: float = -0.075
+var ease_steps: int = Tween.EASE_OUT
+var trans_steps: int = Tween.TRANS_CUBIC
+
+var factor_spr_scale_2 : float = 0.5
+var time_steps_2 : float = 0.75
+var factor_steps_2 : float = 0.75
+
+var colision = false
+
+func _ready() -> void:
+	init_scale = $Sprite2D.scale;
 
 func _physics_process(delta: float) -> void:
 	colision = false
 	if not canwalk: return
 	
 	# VERDE
-	if not greenPressed and Input.is_action_pressed("verde",true):
-		#print_debug("VERDE")
-		Global.trastes[0] = true;
-		greenPressed = true
-		#animated_sprite_2d.play("up")
+	if not Global.trastes[0] and Input.is_action_pressed("verde",true):		
+		#animated_sprite_2d.play("face_down")
 		direction += 1
 		if direction >= len(directions): 
 			direction = 0
 		#print("TURN LEFT")
-	elif Input.is_action_just_released("verde", true):
-		Global.trastes[0] = false
-		greenPressed = false
-		
 	# ROJO
-	if not redPressed and Input.is_action_pressed("rojo",true):
-		#print_debug("ROJO")
-		Global.trastes[1] = true;
-		redPressed = true
+	if not Global.trastes[1] and Input.is_action_pressed("rojo",true):
 		#animated_sprite_2d.play("face_down")
 		direction -= 1
 		if direction < 0: 
 			direction = len(directions) -1
 		#print("TURN RIGHT")
-	elif Input.is_action_just_released("rojo", true):
-		Global.trastes[1] = false
-		redPressed = false
-		
-	# AMARILLO
-	if not yellowPressed and Input.is_action_pressed("amarillo",true):
-		print_debug("AMARILLO")
-		Global.trastes[2] = true
-		yellowPressed = true
-	elif Input.is_action_just_released("amarillo", true):
-		Global.trastes[2] = false
-		yellowPressed = false
-		
-	# AZUL
-	if not bluePressed and Input.is_action_pressed("azul",true):
-		print_debug("AZUL")
-		Global.trastes[3] = true
-		bluePressed = true
-	elif Input.is_action_just_released("azul", true):
-		Global.trastes[3] = false
-		bluePressed = false	
-		
-	# NARANJA
-	if not orangePressed and Input.is_action_pressed("naranja",true):
-		print_debug("NARANJA")
-		Global.trastes[4] = true
-		orangePressed = true
-	elif Input.is_action_just_released("naranja", true):
-		Global.trastes[4] = false
-		orangePressed = false	
-		
 	# RASGEO
 	if not strumPressed and Input.is_action_pressed("rasgar",true):
 		#print_debug("RASGAR")
