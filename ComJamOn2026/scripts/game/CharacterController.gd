12a93335ecf29707cc657c071a13c4c3a1b077e3
Cynthia Tristán <cyntrist@ucm.es>
2026-02-22 18:38:02 +0100

    direcciones???

--- ComJamOn2026/scripts/game/CharacterController.gd
+++ ComJamOn2026/scripts/game/CharacterController.gd
@@ -5,7 +5,7 @@ class_name CharacterController
 
 # N, NE, E, SE, S, SW, W, NW
 var directions := [Vector2(0,1), Vector2(0.75,0.75),Vector2(1,0), Vector2(0.75,-0.75),Vector2(0,-1), Vector2(-0.75,-0.75),Vector2(-1,0), Vector2(-0.75,0.75)]
-@export var sprite_directions : Array[Texture2D] = [null, null, null, null, null, null, null, null]
+@export var sprite_directions : Array[Texture2D] 
 var direction := 0
 	
 var redPressed = false;
@@ -60,10 +60,10 @@ func _physics_process(delta: float) -> void:
 	if not greenPressed and Input.is_action_pressed("verde",true):
 		greenPressed = true
 		#animated_sprite_2d.play("face_down")
-		$Sprite2D.texture = sprite_directions[direction]
 		direction += 1
 		if direction >= len(directions): 
 			direction = 0
+		$Sprite2D.texture = sprite_directions[direction]
 		#print("TURN LEFT")
 	if Input.is_action_just_released("verde", true): greenPressed = false
 		
@@ -71,10 +71,10 @@ func _physics_process(delta: float) -> void:
 	if not redPressed and Input.is_action_pressed("rojo",true):
 		redPressed = true
 		#animated_sprite_2d.play("face_down")
-		$Sprite2D.texture = sprite_directions[direction]
 		direction -= 1
 		if direction < 0: 
 			direction = len(directions) -1
+		$Sprite2D.texture = sprite_directions[direction]
 		#print("TURN RIGHT")
 	if Input.is_action_just_released("rojo", true): redPressed = false
 	# RASGEO
