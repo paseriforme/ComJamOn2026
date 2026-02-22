6bb15d3fc9623f0b0a4a9a78399052665eeed859
Cynthia Tristán <cyntrist@ucm.es>
2026-02-22 16:10:24 +0100

    feedback flechas

--- ComJamOn2026/scripts/intro/key_mapper.gd
+++ ComJamOn2026/scripts/intro/key_mapper.gd
@@ -9,6 +9,8 @@ var actions := ["verde", "rojo", "amarillo", "azul", "naranja", "rasgar"]
 @export var pegar_animations : Array[Animation]
 @onready var animator : AnimationPlayer = $AnimationPlayer
 @export var animators : Array[AnimationPlayer]
+@onready var flecha_confirmar : TextureRect = $Confirmar
+@onready var flecha_reintentar : TextureRect = $Reintentar
 
 func _ready() -> void:
 	next_key()
@@ -16,9 +18,10 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if key >= len(actions): return
 	if Input.is_anything_pressed():
-		InputMap.action_add_event(actions[key], event)
-		print(actions[key], event.get_class())
-		next_key()
+		if key < len(actions):
+			InputMap.action_add_event(actions[key], event)
+			print(actions[key], event.get_class())
+			next_key()
 
 func next_key() -> void:
 	if (key >= 0):
@@ -27,6 +30,15 @@ func next_key() -> void:
 	if (key < len(actions)):
 		buttons[key].visible = true
 		animators[key].play("loop")
+	
+	if key >= len(actions):
+		var tween2 = get_tree().create_tween()
+		tween2.set_ease(Tween.EASE_OUT)
+		tween2.tween_property(flecha_confirmar, "position", Vector2(-20, 0), 1.0).set_trans(Tween.TRANS_ELASTIC)
+		
+		var tween3 = get_tree().create_tween()
+		tween3.set_ease(Tween.EASE_OUT)
+		tween3.tween_property(flecha_reintentar, "position", Vector2(20, 0), 1.0).set_trans(Tween.TRANS_ELASTIC)
 	
 #	for c in range(get_child_count()):
 #		if c == key:
