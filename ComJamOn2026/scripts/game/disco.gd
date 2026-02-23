db0237cfa57847a8b6981c8e63633d083733355d
Andrés García Navarro <andrga37@ucm.es>
2026-02-23 02:31:09 +0100

    aaaaaaaaaaaa

--- ComJamOn2026/scripts/game/disco.gd
+++ ComJamOn2026/scripts/game/disco.gd
@@ -3,8 +3,6 @@ class_name Disco
 
 @export var bpm : float = 120 # pulsos por minuto
 @export var b_x_vuelta : float = 4 # pulsos por vuelta
-@export var desfase: float = 0
-@export var hit_zone_angle: float = 90
 
 var vel:float = 0
 var pause := false
@@ -16,7 +14,7 @@ func end() -> void:
 	#pause = true
 	var tween = get_tree().create_tween()
 	tween.set_ease(Tween.EASE_OUT)
-	tween.tween_property(self, "rotation", deg_to_rad(rotation + desfase), 1.5).set_trans(Tween.TRANS_BACK)
+	tween.tween_property(self, "rotation", deg_to_rad(rotation), 1.5).set_trans(Tween.TRANS_BACK)
 	tween.finished.connect(func(): Global.end_song.emit())
  
 func start() -> void:
