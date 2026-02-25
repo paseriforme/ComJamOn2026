6f7649eccc03b14a597d0a1fbde30a1e1cb7986e
Cynthia Tristán <cyntrist@ucm.es>
2026-02-25 16:01:24 +0100

    hecho

--- ComJamOn2026/scripts/intro/key_mapper.gd
+++ ComJamOn2026/scripts/intro/key_mapper.gd
@@ -15,26 +15,36 @@ var trans : bool = false
 var ini_map
 var somethingpressed = false
 
+var initial_input_map := {}
+
 func _ready() -> void:
 	next_key()
 	Global.sound.set_bgm_volume_db(7)
 	Global.sound.set_sfx_volume_db(7)
 #	Global.sound.play_bgm("ambience", true)
+	if not OS.is_debug_build(): # si es release queremos que no haya nada mapeado pero en debug si para debugear
+		for a in actions:
+			InputMap.action_erase_events(a)
+	_save_initial_inputmap()
 
 func _input(event: InputEvent) -> void:
 #	if key >= len(actions): return
 #	print_debug(event)
-	if event.is_pressed():
+	if event.is_pressed(): # para html que suene pq en el ready no funciona siempre
 		if not Global.sound.bgm.playing:
 			Global.sound.play_bgm("ambience")
 	var raw_val = Input.get_joy_axis(0, 5)
 	if (event.is_pressed() or raw_val != 0.0 and event is InputEventJoypadMotion) and not somethingpressed:
 		somethingpressed = true
-		if key < len(actions):
-			InputMap.action_add_event(actions[key], event)
-			print(actions[key], event.get_class())
-			next_key()
-		else:
+		if key < len(actions): # estás mapeando
+			if not _event_used_before(event): # estas intentando mapear algo ya mapeado
+				InputMap.action_add_event(actions[key], event)
+				print(actions[key], event.get_class())
+				next_key()
+			else:
+				# TODO: mostrar qué está mapeado a lo que estás pulsando
+				pass
+		else: # cuando has acabado de mapear, cuales son tus siguientes acciones?
 			if event.is_action_pressed("rasgar"):
 				_reiniciar()
 			elif event.is_action_pressed("verde"):
@@ -47,11 +57,12 @@ func next_key() -> void:
 		animators[key].play("pegar")
 		Global.sound.play_sfx("duct_tape1", 0.3)
 	key += 1
-	if (key < len(actions)):
+	if (key < len(actions)): 
 		buttons[key].visible = true
 		animators[key].play("loop")
 	
-	if key >= len(actions):
+	if key >= len(actions): # has mapeado la última
+		# animar flechas
 		var tween2 = get_tree().create_tween()
 		tween2.set_ease(Tween.EASE_OUT)
 		tween2.tween_property(flecha_confirmar, "position", Vector2(-20, 0), 1.0).set_trans(Tween.TRANS_ELASTIC)
@@ -73,13 +84,17 @@ func _confirmar():
 func _reiniciar():
 	if trans: return
 	for a in actions:
-		for e in InputMap.action_get_events(a):
-			InputMap.action_erase_event(a, e)
-	_remap()
+		InputMap.action_erase_events(a)
+#		for e in InputMap.action_get_events(a):
+#			InputMap.action_erase_event(a, e)
+	_restore_initial_inputmap()
+	
 	key = -1
 	for button in buttons:
 		button.visible = false
 	next_key()
+	
+	# devolver flechas adónde estaban
 	var tween2 = get_tree().create_tween()
 	tween2.set_ease(Tween.EASE_OUT)
 	tween2.tween_property(flecha_confirmar, "position", Vector2(-450.0, 0), 1.0).set_trans(Tween.TRANS_ELASTIC)
@@ -92,6 +107,24 @@ func _reiniciar():
 	Global.play_cardboard(0.2)
 	pass
 
-func _remap():
-#	InputMap.action_add_event("")
-	pass
+func _event_used_before(new_event: InputEvent) -> bool:
+	for action in actions:
+		for e in InputMap.action_get_events(action):
+			if e.is_match(new_event):
+				return true
+	return false
+	
+func _save_initial_inputmap():
+	initial_input_map.clear()
+
+	for action in actions:
+		initial_input_map[action] = []
+		for e in InputMap.action_get_events(action):
+			initial_input_map[action].append(e.duplicate())
+	
+func _restore_initial_inputmap():
+	for action in actions:
+		InputMap.action_erase_events(action)
+
+		for e in initial_input_map[action]:
+			InputMap.action_add_event(action, e)
