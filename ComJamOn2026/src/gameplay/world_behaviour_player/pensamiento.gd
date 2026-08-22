86c26cb4b278f67d213c05adbd0c16e89944eac4
Andrés García Navarro <andrga37@ucm.es>
2026-08-22 21:09:24 +0200

    movimiento y pensamiento refacto

--- ComJamOn2026/src/gameplay/world_behaviour_player/pensamiento.gd
+++ ComJamOn2026/src/gameplay/world_behaviour_player/pensamiento.gd
@@ -4,8 +4,7 @@ class_name Pensamiento
 @export var control_feedback_time 	: float = 1.0
 @export var animator				: AnimationPlayer
 
-var mostrando : bool = false
-var control_current_time : float = 0.0
+var control_current_time	: float = 0.0
 
 func _ready() -> void:
 	control_current_time = control_feedback_time
@@ -14,17 +13,32 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	control_current_time += delta
 	
-	if (Input.is_anything_pressed()):
+	if _accion_de_juego_pulsada():
 		control_current_time = 0.0
-		mostrando = false
+		animator.play("despensamiento")
+		visible = false
 	
-	if control_current_time >= control_feedback_time and not mostrando:
+	if control_current_time >= control_feedback_time and not visible:
 		control_current_time = 0.0
 		animator.play("pensamiento")
-		mostrando = true
+		visible = true
 		Global.play_paper(0.1)
-		pass
+
+func _accion_de_juego_pulsada() -> bool:
+	return (
+		Input.is_action_just_pressed("verde")
+		or Input.is_action_just_pressed("rojo")
+		or Input.is_action_just_pressed("amarillo")
+		or Input.is_action_just_pressed("azul")
+		or Input.is_action_just_pressed("naranja")
+		or Input.is_action_just_pressed("rasgar")
+	)
 
 func _on_animation_finished(anim_name: StringName) -> void:
 	if (anim_name == "pensamiento"):
 		animator.play("loop")
+
+## Llamado por el animator player
+func pensamiento_step_sound():
+	#Global.play_cardboard(0.2)
+	SoundSystem.play_sfx("step", 0.2)
--- ComJamOn2026/src/gameplay/world_behaviour_player/world_player_controller.gd
+++ ComJamOn2026/src/gameplay/world_behaviour_player/world_player_controller.gd
@@ -1,92 +1,69 @@
 extends RigidBody2D
-class_name CharacterController
+class_name WorldPlayerController
 
 @export var speed : float = 10
+@export var sprite_directions 	: Array[Texture2D] 
 
 # N, NE, E, SE, S, SW, W, NW NOOOO
 # S SW W NW N NE E SE
-var directions := [Vector2(0,1), Vector2(0.75,0.75),Vector2(1,0), Vector2(0.75,-0.75),Vector2(0,-1), Vector2(-0.75,-0.75),Vector2(-1,0), Vector2(-0.75,0.75)]
-@export var sprite_directions : Array[Texture2D] 
-var direction := 0
+var directions	:= [Vector2(0,1), Vector2(0.75,0.75),Vector2(1,0), 
+					Vector2(0.75,-0.75),Vector2(0,-1), Vector2(-0.75,-0.75),
+					Vector2(-1,0), Vector2(-0.75,0.75)]
+var direction 	:= 0
 
-var redPressed = false;
-var greenPressed = false;
-var strumPressed = false;
-var canwalk = false;
+# flags
+var canwalk 	:= true
 
 var init_scale;
-var factor_spr_scale : Vector2 = Vector2(0.2, 0.2);
-var trans_scale = Tween.TRANS_QUAD
+var factor_spr_scale 	:= Vector2(0.2, 0.2)
+var trans_scale 		:= Tween.TRANS_QUAD
 
-var time_steps: float = 0.3
-var factor_steps: float = -0.075
-var ease_steps: int = Tween.EASE_OUT
-var trans_steps: int = Tween.TRANS_CUBIC
+var time_steps			: float = 0.3
+var factor_steps		: float = -0.075
+var ease_steps			: int = Tween.EASE_OUT
+var trans_steps			: int = Tween.TRANS_CUBIC
 
-var factor_spr_scale_2 : float = 0.5
-var time_steps_2 : float = 0.75
-var factor_steps_2 : float = 0.75
+var factor_spr_scale_2 	: float = 0.5
+var time_steps_2 		: float = 0.75
+var factor_steps_2 		: float = 0.75
 
-var colision = false
 
-@onready var animator : AnimationPlayer = $AnimationPlayer 
+@onready var animator 	: AnimationPlayer = $AnimationPlayer 
 
 func _ready() -> void:
-	init_scale = $Sprite2D.scale;
+	init_scale = $Sprite2D.scale
+	body_entered.connect(_rebote)
 
 func _physics_process(delta: float) -> void:
-	
-	colision = false
 	if not canwalk: return
 	
-#	if (Input.is_anything_pressed()): # se ha pulsado algo
-	if not strumPressed and Input.is_action_pressed("rasgar",true): 
-		animator.play("despensamiento")
-#		print_debug(">>> HAS PULSADO ALGO")
-#		Global.play_paper(0.1)
-		
 	# VERDE
-	if not greenPressed and Input.is_action_pressed("verde",true):
-		greenPressed = true
-		#animated_sprite_2d.play("face_down")
-		direction += 1
-		if direction >= len(directions): 
-			direction = 0
-		$Sprite2D.texture = sprite_directions[direction]
-#		Global.play_paper(0.2)
+	if Input.is_action_just_pressed("verde", true):
 		#print("TURN LEFT")
-	if Input.is_action_just_released("verde", true): greenPressed = false
-		
-	# ROJO
-	if not redPressed and Input.is_action_pressed("rojo",true):
-		redPressed = true
-		#animated_sprite_2d.play("face_down")
-		direction -= 1
-		if direction < 0: 
-			direction = len(directions) -1
+		direction = (direction +1) % len(directions)
 		$Sprite2D.texture = sprite_directions[direction]
-#		Global.play_paper(0.2)
+		Global.play_paper(0.2)
+	# ROJO
+	if Input.is_action_just_pressed("rojo", true):
 		#print("TURN RIGHT")
-	if Input.is_action_just_released("rojo", true): redPressed = false
+		direction = (direction -1) % len(directions)
+		$Sprite2D.texture = sprite_directions[direction]
+		Global.play_paper(0.2)
 	# RASGEO
-	if not strumPressed and Input.is_action_pressed("rasgar",true):
+	if Input.is_action_just_pressed("rasgar",true):
 		#print_debug("RASGAR")
 		var velocity = directions[direction] * speed
-		# ANDRES AQUI
-		strumPressed = true
 		apply_impulse(velocity)
 		Global.direccion_jugador = velocity
 		SoundSystem.play_sfx("carton3", 0.2)
 #		SoundSystem.play_sfx("step", 0.2)
 		#print("MOVE: ", directions[direction])
-	elif Input.is_action_just_released("rasgar", true):
-		strumPressed = false
-		
-	var colisiones: Array[Node2D] = get_colliding_bodies();
-	if colisiones.size() > 0:
-		colision = true
 
 func _callback_XD():
+	var velocity = directions[direction] * speed
+	velocity *= -0.5
+	apply_impulse(velocity)
+	
 	# second
 	var time = time_steps * time_steps_2
 	# second scale
@@ -95,20 +72,14 @@ func _callback_XD():
 	tween.tween_property($Sprite2D, "scale", init_scale + factor_spr_scale * factor_spr_scale_2, time/2).set_trans(trans_scale)
 	tween.set_ease(Tween.EASE_IN)
 	tween.tween_property($Sprite2D, "scale", init_scale, time/2).set_trans(trans_scale)
-
-	# second steps
-	var velocity = directions[direction] * speed * factor_steps_2 * factor_steps
-	var tween2 = get_tree().create_tween()
-	tween2.set_ease(ease_steps)
-	tween2.tween_property(self, "position", position + velocity, time).set_trans(trans_steps)
-	#tween2.finished.connect(func(): Global.chocar_npc.emit())
+	#tween.finished.connect(func(): Global.chocar_npc.emit())
 	SoundSystem.play_sfx("bounce", 0.2)
 
 func _rebote(body: Node) -> void:
 	#version por física
-#		var velocity = directions[direction] * speed
-#		velocity *= -0.5
-#		apply_impulse(velocity)
+	var velocity = directions[direction] * speed
+	velocity *= -0.5
+	apply_impulse(velocity)
 	
 	#escalado
 	var tween = get_tree().create_tween()
@@ -116,11 +87,6 @@ func _rebote(body: Node) -> void:
 	tween.tween_property($Sprite2D, "scale", init_scale + factor_spr_scale, time_steps/2).set_trans(trans_scale)
 	tween.set_ease(Tween.EASE_IN)
 	tween.tween_property($Sprite2D, "scale", init_scale, time_steps/2).set_trans(trans_scale)
+	tween.finished.connect(_callback_XD)
 	
-	#rebote
-	var velocity = directions[direction] * speed * factor_steps
-	var tween2 = get_tree().create_tween()
-	tween2.set_ease(ease_steps)
-	tween2.tween_property(self, "position", position + velocity, time_steps).set_trans(trans_steps)
-	tween2.finished.connect(_callback_XD)
 	SoundSystem.play_sfx("bounce", 0.2)
